f1a9349ece5c28fef40fe412fb43239c7b353a56
JakeGreer <Jakegreer11@gmail.com>
2017-11-02 18:50:27 -0700

    switched databases

--- controllers/articleController.js
+++ controllers/articleController.js
@@ -15,6 +15,12 @@ module.exports = {
       .then(dbModel => res.json(dbModel))
       .catch(err => res.status(422).json(err));
   },
+  findBySaved: function(req, res) {
+    db.Article
+      .findBySaved(req.params.saved)
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.status(422).json(err));
+  },
   create: function(req, res) {
     db.Article
       .create(req.body)
